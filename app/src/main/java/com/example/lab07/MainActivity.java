1b645db50feaba6c1b326b75f14b519deb8e98c9
Aklima2004 <iskakbaeva.aklima@mail.ru>
2025-03-30 03:40:02 +0500

    Adding functionality for user login

--- app/src/main/java/com/example/lab07/MainActivity.java
+++ app/src/main/java/com/example/lab07/MainActivity.java
@@ -1,24 +1,65 @@
 package com.example.lab07;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edUsername, edPassword;
+    private Button btnLogin, btnSignUp;
+    private final String CREDENTIAL_SHARED_PREF = "our_shared_pref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Инициализация элементов UI
+        edUsername = findViewById(R.id.ed_username);
+        edPassword = findViewById(R.id.ed_password);
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignUp = findViewById(R.id.btn_signup);
+
+        // Переход на страницу регистрации
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Переход на экран регистрации
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Логика авторизации
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
+                String strUsername = credentials.getString("Username", null);
+                String strPassword = credentials.getString("Password", null);
+
+                String username_from_ed = edUsername.getText().toString();
+                String password_from_ed = edPassword.getText().toString();
+
+                if (strUsername != null && strUsername.equals(username_from_ed)) {
+                    if (strPassword != null && strPassword.equals(password_from_ed)) {
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, FileActivity.class); // Переход на экран работы с файлами
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
